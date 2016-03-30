9d97329fb82957753a50c6909727be3add6ac209
Hieu Pious <hieupious@ymail.com>
2016-03-30 20:01:23 +0700

    Finish milestone 4

--- AnimationLab/ViewController.swift
+++ AnimationLab/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
     var trayOriginalCenter: CGPoint!
     var trayCenterWhenOpen: CGPoint!
     var trayCenterWhenClosed: CGPoint!
+    var newlyCreatedFace: UIImageView!
+    var newlyFaceCenter: CGPoint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -69,5 +71,36 @@ class ViewController: UIViewController {
         })
     }
 
+    @IBAction func onPanSmileyGesture(panGestureRecognizer: UIPanGestureRecognizer) {
+        let translation = panGestureRecognizer.translationInView(view)
+        
+        if panGestureRecognizer.state == UIGestureRecognizerState.Began {
+            
+            // Gesture recognizers know the view they are attached to
+            let imageView = panGestureRecognizer.view as! UIImageView
+            
+            // Create a new image view that has the same image as the one currently panning
+            newlyCreatedFace = UIImageView(image: imageView.image)
+            
+            // Add the new face to the tray's parent view.
+            view.addSubview(newlyCreatedFace)
+            
+            // Initialize the position of the new face.
+            newlyCreatedFace.center = imageView.center
+            
+            // Since the original face is in the tray, but the new face is in the
+            // main view, you have to offset the coordinates
+            newlyCreatedFace.center.y += trayView.frame.origin.y
+            newlyFaceCenter = newlyCreatedFace.center
+        } else if panGestureRecognizer.state == UIGestureRecognizerState.Changed {
+            
+            newlyCreatedFace.center = CGPoint(x: newlyFaceCenter.x + translation.x, y: newlyFaceCenter.y + translation.y)
+            
+        } else if panGestureRecognizer.state == UIGestureRecognizerState.Ended {
+
+            
+            
+        }
+    }
 }
 
