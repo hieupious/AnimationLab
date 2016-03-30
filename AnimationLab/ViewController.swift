f3d34f4b54fdf615d9a0ba1713260f8e50821b0a
Hieu Pious <hieupious@ymail.com>
2016-03-30 19:48:22 +0700

    Finish milestone 3

--- AnimationLab/ViewController.swift
+++ AnimationLab/ViewController.swift
@@ -12,11 +12,16 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var trayView: UIView!
     var trayOriginalCenter: CGPoint!
+    var trayCenterWhenOpen: CGPoint!
+    var trayCenterWhenClosed: CGPoint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         //trayOriginalCenter = trayView.center
+        trayCenterWhenOpen = CGPoint(x: 0, y: 320)
+        trayCenterWhenClosed = CGPoint(x: 0, y: 520)
+        trayView.frame.origin = trayCenterWhenClosed
     }
 
     override func didReceiveMemoryWarning() {
@@ -27,6 +32,7 @@ class ViewController: UIViewController {
     @IBAction func onTrayPanGesture(panGestureRecognizer: UIPanGestureRecognizer) {
         let point = panGestureRecognizer.locationInView(trayView)
         let translation = panGestureRecognizer.translationInView(view)
+        let velocity = panGestureRecognizer.velocityInView(view)
         
         if panGestureRecognizer.state == UIGestureRecognizerState.Began {
             print("Gesture began at: \(point)")
@@ -38,9 +44,29 @@ class ViewController: UIViewController {
         } else if panGestureRecognizer.state == UIGestureRecognizerState.Ended {
             print("Gesture ended at: \(point)")
 //            trayView.center = trayOriginalCenter
+            UIView.animateWithDuration(0.5, animations: {
+                if velocity.y > 0 {
+                    self.trayView.frame.origin = self.trayCenterWhenClosed
+                } else {
+                    self.trayView.frame.origin = self.trayCenterWhenOpen
+                }
+            })
+            
+            
         }
         
         
+    }
+    
+    
+    @IBAction func onTapTrayViewGesture(sender: UITapGestureRecognizer) {
+        UIView.animateWithDuration(0.5, animations: {
+            if self.trayView.frame.origin == self.trayCenterWhenOpen {
+                self.trayView.frame.origin = self.trayCenterWhenClosed
+            } else {
+                self.trayView.frame.origin = self.trayCenterWhenOpen
+            }
+        })
     }
 
 }
